291c66266e6df3cb026b820ef4b1da57903e3d39
Suhayel <a.suhail@aol.com>
2019-07-19 18:05:31 +0300

    Adding song play / pause function

--- app/src/main/java/android/example/musicplayerapp/MainActivity.java
+++ app/src/main/java/android/example/musicplayerapp/MainActivity.java
@@ -1,13 +1,42 @@
 package android.example.musicplayerapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button playBytton, pauseButton;
+    private MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playBytton = (Button) findViewById(R.id.playButton);
+        pauseButton = (Button) findViewById(R.id.pauseButton);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.song1);
+
+        playBytton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Playing sound",Toast.LENGTH_SHORT).show();
+                mediaPlayer.start();
+            }
+        });
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Pausing sound",Toast.LENGTH_SHORT).show();
+                mediaPlayer.pause();
+            }
+        });
+
     }
+
 }
