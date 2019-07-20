5d3a6f2202f81d784d545491c95e55032bd1dd73
Suhayel <a.suhail@aol.com>
2019-07-20 15:27:02 +0300

    Adding on Completion Listener

--- app/src/main/java/android/example/musicplayerapp/MainActivity.java
+++ app/src/main/java/android/example/musicplayerapp/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button playBytton, pauseButton;
     private MediaPlayer mediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,15 +25,24 @@ public class MainActivity extends AppCompatActivity {
         playBytton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Playing sound",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Playing sound", Toast.LENGTH_SHORT).show();
                 mediaPlayer.start();
+                // code added for onCompletionListener
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                    Toast.makeText(MainActivity.this,"I'm done",Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                // code added for onCompletionListener
             }
         });
 
         pauseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Pausing sound",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Pausing sound", Toast.LENGTH_SHORT).show();
                 mediaPlayer.pause();
             }
         });
